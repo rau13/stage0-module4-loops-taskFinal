298aa08deaf588caaee25e84540a24376e74824f
rau13 <rauan5005@gmail.com>
2022-08-15 15:41:03 +0600

    first commit 15.08

--- src/main/java/school/mjc/stage0/loops/finalTask/HalfPyramid.java
+++ src/main/java/school/mjc/stage0/loops/finalTask/HalfPyramid.java
@@ -2,7 +2,16 @@ package school.mjc.stage0.loops.finalTask;
 
 public class HalfPyramid {
     public void printHalfPyramid(int cathetusLength) {
-
+        int rows = cathetusLength;
+        for (int i = rows; i > 0; i--) {
+            for (int s = 1; s < i; s++) {
+                System.out.print(" ");
+            }
+            for (int j = rows; j >= i; j--) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
     }
 
     public static void main(String[] args) {
--- src/main/java/school/mjc/stage0/loops/finalTask/Pyramid.java
+++ src/main/java/school/mjc/stage0/loops/finalTask/Pyramid.java
@@ -2,7 +2,19 @@ package school.mjc.stage0.loops.finalTask;
 
 public class Pyramid {
     public void printPyramid(int cathetusLength) {
+        for (int i = 1; i <= cathetusLength; i++) {              // loop row
 
+            for (int j = 1; j <= cathetusLength - i; j++)        // loop column (Triangle one)
+                System.out.print("  ");
+
+            for (int k = i; k >= 1; k--)            // loop column (Triangle two)
+                System.out.print(  " " + k);
+
+            for (int k = 2; k <=i; k++)             // loop column (Triangle three)
+                System.out.print( " " + k);
+
+            System.out.println();                   // used to go on new line (new row)
+        }
     }
 
     public static void main(String[] args) {
